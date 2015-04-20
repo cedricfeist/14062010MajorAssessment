34bc646466340f050dff89427ffab636003f33e5
jc259867 <Cedric Feist>
2015-04-20 17:32:35 +1000

    Stuff from the Prac
    Started Image Upload etc

--- artistinfo.php
+++ artistinfo.php
@@ -7,13 +7,29 @@ include("artistsconnect.php");
 <html>
 <head>
     <meta charset="UTF-8">
-                   </head>
+    
+</head>
 
+    <body>
+<?php 
+$sql = "SELECT * FROM artists WHERE id='$_GET[tag]'";
+var_dump ($dbh);
+echo"<br>";
 
+echo $sql;
+echo"<br>";
 
+$results = $dbh->query($sql);
+echo"<br>";
+var_dump ($results);
 
+printf("Name:%s <br>", $results[name]);
 
+$dbh = null;
 
+?>
+
+    </body>
 
 
 </html>
